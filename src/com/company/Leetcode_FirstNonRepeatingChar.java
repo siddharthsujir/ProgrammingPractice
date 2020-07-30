beea4ade14370b5a24953ff1988c30e92a57d9c6
unknown <shivaa.siddharth@gmail.com>
2020-07-30 21:52:35 +0530

    second commit on 07303030

---
+++ src/com/company/Leetcode_FirstNonRepeatingChar.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Leetcode_FirstNonRepeatingChar {
+
+    public int firstUniqChar(String s) {
+
+        HashMap<Character,Integer> hm= new HashMap<>();
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(!hm.containsKey(s.charAt(i)))
+                hm.put(s.charAt(i),1);
+            else
+                hm.put(s.charAt(i),hm.get(s.charAt(i))+1);
+        }
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(hm.get(s.charAt(i))==1)
+                return i;
+
+        }
+
+
+
+    }
+}
