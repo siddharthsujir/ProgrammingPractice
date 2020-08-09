540a37f4c8f263ed2d49473f7c3a74d2ba4a7d00
unknown <shivaa.siddharth@gmail.com>
2020-08-09 22:32:18 +0530

    08092020 unsuccessful

---
+++ src/com/company/HackerRank_RemoveElement.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class HackerRank_RemoveElement {
+
+    public static int removeElement(int[] nums, int val) {
+
+        int j=0;
+        int count=0;
+        int i=0;
+        while(i<nums.length && j<nums.length)
+        {
+            if(nums[i]==val)
+            {
+                nums[i]=nums[j++];
+                count=count+1;
+            }
+            else
+            {
+                i++;
+                j++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
