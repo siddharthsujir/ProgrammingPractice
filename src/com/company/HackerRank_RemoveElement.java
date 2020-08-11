418677b8f4dc65137226a25d8f4c059cb2031b3b
unknown <shivaa.siddharth@gmail.com>
2020-08-11 18:08:03 +0530

    08112020

--- src/com/company/HackerRank_RemoveElement.java
+++ src/com/company/HackerRank_RemoveElement.java
@@ -4,27 +4,53 @@ public class HackerRank_RemoveElement {
 
     public static int removeElement(int[] nums, int val) {
 
-        int j=0;
-        int count=0;
-        int i=0;
+        int i=0,j=0,count=0;
+
+        if(nums.length==0)
+            return 0;
+        if(nums.length==1)
+        {
+            if(nums[0]==val)
+                return 0;
+        }
+
         while(i<nums.length && j<nums.length)
         {
+
             if(nums[i]==val)
             {
-                nums[i]=nums[j++];
-                count=count+1;
+                if(nums[j]==val)
+                    j++;
+                else
+                {
+                    nums[i]=nums[j];
+                    count++;
+                    i++;
+                    j++;
+                }
+
             }
             else
             {
+                nums[i]=nums[j];
                 i++;
-                if(i==j)
-                    j++;
+                j++;
             }
+
         }
-        return count;
+
+//        for(int k=0;k<nums.length;k++)
+//        {
+//            System.out.print(nums[k]+" ");
+//        }
+        if(i>0 && i==nums.length && nums[i-1]==val)
+            count=count+1;
+        return nums.length-count;
+
     }
     public static void main(String[] args)
     {
-
+        int[] arr={3,3};
+        System.out.println(removeElement(arr,3));
     }
 }
