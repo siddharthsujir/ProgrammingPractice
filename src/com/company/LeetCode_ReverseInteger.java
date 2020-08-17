3a7011bdea2b405b60fa52b14abb183ff90e4d81
unknown <shivaa.siddharth@gmail.com>
2020-08-17 21:25:29 +0530

    successful 08172020

--- src/com/company/LeetCode_ReverseInteger.java
+++ src/com/company/LeetCode_ReverseInteger.java
@@ -5,7 +5,7 @@ public class LeetCode_ReverseInteger {
     public static int reverse(int x) {
 
         int no=0;
-        int newNo=0;
+        long newNo=0;
         boolean isNegative=false;
         System.out.println((int)x);
         if(x!=(int)x)
@@ -30,12 +30,23 @@ public class LeetCode_ReverseInteger {
 
             x=x/10;
         }
-//        if(newNo!= (int)newNo)
-//            return 0;
-        if(isNegative)
-            return newNo*-1;
-        else
-            return newNo;
+        try
+        {
+            if(newNo!= (int)newNo)
+                return 0;
+            else
+            {
+                if(isNegative)
+                    return (int)newNo*-1;
+                else
+                    return (int)newNo;
+            }
+        }
+        catch(Exception e)
+        {
+            return 0;
+        }
+
     }
 
     public static void main(String[] args)
