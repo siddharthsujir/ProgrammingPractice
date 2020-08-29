524f40a9aa295eeb3f23a5271a0b918f48f01f20
unknown <shivaa.siddharth@gmail.com>
2020-08-29 21:47:46 +0530

    08292020 unsucessfull

---
+++ src/com/company/HackerRank_CatAndMouse.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class HackerRank_CatAndMouse {
+    static String catAndMouse(int x, int y, int z) {
+        int catA=0;
+        int catB=0;
+
+        catA=z>x?z-x:x-z;
+        catB=z>y?z-y:y-z;
+
+        if(catA>catB)
+            return  "Cat B";
+        else if (catB<catA)
+            return  "Cat A";
+        else if(catA==catB)
+            return  "Mouse C";
+
+
+    }
+
+}
