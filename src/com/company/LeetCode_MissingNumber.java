c61f00579c1dadded4c0b58955098d8d1230e00c
unknown <shivaa.siddharth@gmail.com>
2020-09-03 21:57:58 +0530

    09032020

---
+++ src/com/company/LeetCode_MissingNumber.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class LeetCode_MissingNumber {
+
+    public static int missingNumber(int[] nums) {
+        int max=-1;
+        int min=Integer.MAX_VALUE;
+        int sum=0;
+        int arrSum=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>max)
+                max=nums[i];
+            if(nums[i]<min)
+                min=nums[i];
+
+            arrSum=arrSum+nums[i];
+        }
+
+        if(min!=0)
+            return 0;
+
+        for(int i=0;i<=max;i++)
+        {
+            sum=sum+i;
+        }
+
+
+        return sum-arrSum;
+
+
+    }
+
+    public static void main(String[] args)
+    {
+
+        int[] arr={3,0,1};
+        System.out.println(missingNumber(arr));
+    }
+}
