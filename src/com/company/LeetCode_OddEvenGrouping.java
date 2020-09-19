2868212d20a1275cc5462e7dfc28ab61b26561a7
unknown <shivaa.siddharth@gmail.com>
2020-09-19 09:54:15 +0530

    09192020 odd even linkedList

--- src/com/company/LeetCode_OddEvenGrouping.java
+++ src/com/company/LeetCode_OddEvenGrouping.java
@@ -1,10 +1,11 @@
 package com.company;
 
 
+import java.util.List;
 
 public class LeetCode_OddEvenGrouping {
 
-    public ListNode oddEvenList(ListNode head) {
+    public ListNode oddEvenList2(ListNode head) {
 
         ListNode temp= head;
         ListNode even=null;
@@ -40,8 +41,79 @@ public class LeetCode_OddEvenGrouping {
         return head;
     }
 
+    public static ListNode oddEvenList(ListNode head)
+    {
+        if(head==null)
+            return null;
+
+        ListNode firstPtr=head;
+        ListNode secondPtr=head;
+        int length=1;
+        int i=1;
+        while (secondPtr.next!=null)
+        {
+            length++;
+            secondPtr=secondPtr.next;
+        }
+
+        if(length==1 || length==2)
+            return head;
+        while (i<length)
+        {
+            ListNode temp=firstPtr.next;
+            firstPtr.next=temp.next;
+            temp.next=null;
+
+            secondPtr.next=temp;
+            secondPtr=secondPtr.next;
+            i+=2;
+            firstPtr=firstPtr.next;
+        }
+        return  head;
+    }
+
+    public static ListNode addToOutputList(ListNode output, int val)
+    {
+        if(output==null)
+            output=new ListNode(val);
+        else
+        {
+            ListNode temp=output;
+            while (temp.next!=null)
+            {
+                temp=temp.next;
+            }
+            temp.next=new ListNode(val);
+        }
+        return output;
+    }
+
+    public static void printLL(ListNode node)
+    {
+        while (node!=null)
+        {
+            System.out.println(node.val);
+            node=node.next;
+        }
+    }
     public static void main(String[] args)
     {
+
+        int[] arr={1,1};
+        int i=0;
+        ListNode node=null;
+        while (i<arr.length)
+        {
+            node=addToOutputList(node,arr[i]);
+            i++;
+        }
+
+        printLL(node);
+
+        node=oddEvenList(node);
+        System.out.println("After Odd Even Grouping!");
+
+        printLL(node);
 
     }
 }
