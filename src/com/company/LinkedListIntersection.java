bb43c6a4c9549c36e07e9379de5b7887f7fdd4fe
unknown <shivaa.siddharth@gmail.com>
2020-10-27 21:30:23 +0530

    10272020 first commit successful

--- src/com/company/LinkedListIntersection.java
+++ src/com/company/LinkedListIntersection.java
@@ -1,4 +1,19 @@
 package com.company;
 
+
+   class ListNode2 {
+      int val;
+      ListNode next;
+      ListNode2(int x) { val = x; }
+  }
+
 public class LinkedListIntersection {
+
+    public void deleteNode(ListNode node) {
+
+        ListNode temp=node.next;
+        node.val=temp.val;
+        node.next=temp.next;
+        temp=null;
+    }
 }
