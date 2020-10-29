c0c7cbeb4a03a986740ef7f5881853cba1f44fa3
unknown <shivaa.siddharth@gmail.com>
2020-10-29 22:07:49 +0530

    10292020 accepted hacker rank soln

--- src/com/company/LLImpl.java
+++ src/com/company/LLImpl.java
@@ -254,6 +254,28 @@ public class LLImpl {
        // reversePrint(node);
     }
 
+    static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+
+        if(head1==null && head2==null)
+            return true;
+        if((head1==null && head2!=null) || (head2==null&& head1!=null))
+            return false;
+
+
+        while(head1!=null && head2!=null)
+        {
+            if(head1.data!=head2.data)
+                return false;
+            head1=head1.next;
+            head2=head2.next;
+        }
+
+        if(head1!=null || head2!=null)
+            return false;
+
+    return  true;
+    }
+
 }
 
 
