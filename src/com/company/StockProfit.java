d16e744aa7b20a36868d031ab767a6588bc136fe
unknown <shivaa.siddharth@gmail.com>
2020-10-31 19:37:37 +0530

    10312020 max profit accepted optimal solution

--- src/com/company/StockProfit.java
+++ src/com/company/StockProfit.java
@@ -4,8 +4,8 @@ public class StockProfit {
 
     public static void main(String[] args)
     {
-        int[] arr={10,7,5,8,11,9};
-        System.out.println(maxProfit(arr));
+        int[] arr={1,2};
+        System.out.println(maxProfit2(arr));
     }
 
     public static int maxProfit(int[] arr)
@@ -17,6 +17,27 @@ public class StockProfit {
             for(int j=i+1;j<arr.length;j++)
             {
                 int diff=arr[j]-arr[i];
+                if(diff>maxProfit)
+                    maxProfit=diff;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static int maxProfit2(int[] arr)
+    {
+        if (arr.length==0)
+            return 0;
+        int maxProfit=0;
+        int maxElement=arr[arr.length-1];
+
+        for(int i=arr.length-2;i>=0;i--)
+        {
+            if(arr[i]>maxElement)
+                maxElement=arr[i];
+            else
+            {
+                int diff=maxElement-arr[i];
                 if(diff>maxProfit)
                     maxProfit=diff;
             }
