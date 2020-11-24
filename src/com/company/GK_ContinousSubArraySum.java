d64fc6e0962a38651461d309645b97c342c35efd
unknown <shivaa.siddharth@gmail.com>
2020-11-24 07:01:47 +0530

    Geek for geeks pblm on arrays working sln

--- src/com/company/GK_ContinousSubArraySum.java
+++ src/com/company/GK_ContinousSubArraySum.java
@@ -6,12 +6,14 @@ public class GK_ContinousSubArraySum {
     {
         int sum=0;
         int i=0,j=0,len=arr.length;
+        int flag=0;
         while (i<len && j<len)
         {
-
-            if(sum==target)
+            sum=sum+arr[j];
+            if(checkTarget(sum,target))
             {
                 System.out.println("start="+(i+1)+" end="+(j+1));
+                flag=1;
                 break;
             }
 
@@ -21,13 +23,33 @@ public class GK_ContinousSubArraySum {
             {
                 sum=sum-arr[i];
                 i++;
+                if(checkTarget(sum,target))
+                {
+                    System.out.println("start="+(i+1)+" end="+(j+1));
+                    flag=1;
+                    break;
+                }
+
+
             }
-            sum=sum+arr[j];
+
         }
+
+        if(flag==0)
+            System.out.println("start="+(-1)+" end="+(-1)); ;
+    }
+
+    public static boolean checkTarget(int sum,int target)
+    {
+        if(sum==target)
+        {
+           return true;
+        }
+        return false;
     }
     public static void main(String[] args)
     {
-        int[] arr={1,2,3,7,5};
-        findContinousArraySum(arr,12);
+        int[] arr={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};//{1,2,3,7,5};
+        findContinousArraySum(arr,100);
     }
 }
