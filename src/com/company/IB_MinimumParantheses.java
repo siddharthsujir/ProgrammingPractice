1c2c7776bc659d991fc2675531ffd6fb03350a1b
unknown <shivaa.siddharth@gmail.com>
2021-01-21 21:22:04 +0530

    01122021 minimum parantheses

--- src/com/company/IB_MinimumParantheses.java
+++ src/com/company/IB_MinimumParantheses.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class IB_MinimumParantheses {
 
     public static void main(String[] args) {
-
+            System.out.println(countParantheses(")))))("));
     }
 
     public static int countParantheses(String s)
@@ -13,6 +13,26 @@ public class IB_MinimumParantheses {
         int count=0;
         Stack<Character> stack=new Stack<>();
 
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+                stack.push('(');
+            else
+            {
+                if(stack.isEmpty())
+                    stack.push(')');
+                else
+                {
+                    if(stack.peek()=='(')
+                        stack.pop();
+                    else
+                        stack.push(')');
+                }
+
+            }
+        }
+
+        count=stack.size();
 
         return count;
     }
