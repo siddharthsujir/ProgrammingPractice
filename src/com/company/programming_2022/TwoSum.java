3f223cd060f5e2ad620ac9a5942dc517b093a47e
siddharthsujir <siddharthsujir.mohan@zoom.us>
2022-01-16 18:48:45 +0530

    First commit

--- src/com/company/programming_2022/TwoSum.java
+++ src/com/company/programming_2022/TwoSum.java
@@ -3,6 +3,6 @@ package com.company.programming_2022;
 public class TwoSum {
     public static void main(String[] args)
     {
-        System.out.println("Hello World ");
+        System.out.println("Hello Worlds ");
     }
 }
